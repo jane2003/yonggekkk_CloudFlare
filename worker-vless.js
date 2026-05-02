276f5914cdcadc36a45a8958313ab47a235e21f0
jane2003 <47921342+jane2003@users.noreply.github.com>
2026-05-02 13:35:23 +0800

    Modify userID comment for clarity
    Updated userID variable comment to indicate UUID change.

--- worker-vless.js
+++ worker-vless.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'b002dfa7-824c-4983-b0ee-376386566596';
+let userID = 'b002dfa7-824c-4983-b0ee-376386566596';//此处必须改为自己的uuid。
 
 const proxyIPs = ['cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
