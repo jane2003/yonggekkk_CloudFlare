78a392121902910592e734be90531f219e90d12c
jane2003 <47921342+jane2003@users.noreply.github.com>
2026-05-02 13:23:35 +0800

    Update userID to a new UUID

--- _worker.js
+++ _worker.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '9782ea94-2fca-4fdd-be4e-8b5ee7a1dcc3';
 
 let proxyIP = 'cdn.xn--b6gac.eu.org';
 
